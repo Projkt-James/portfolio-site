72f1ba8dc879bb4e78385970cc74c9dc17982919
James <James@projkt.io>
2016-09-16 16:18:31 +1000

    Completed gulp workflow integration

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,30 @@
+'use strict';
+
 var gulp = require('gulp');
 
-gulp.task('default', function(){
-    console.log('Hello world');
+function handleError(error) {
+    console.log(error);
+    this.emit('end');
+}
+
+// Style Task
+// Compiles Sass Code to css directory 
+gulp.task('styles', function() {
+    var sass = require('gulp-sass');
+    var autoprefixer = require('gulp-autoprefixer');
+
+    gulp.src('scss/**/*.scss')
+        .pipe(sass()).on('error', handleError)
+        .pipe(autoprefixer('> 5%'))
+        .pipe(gulp.dest('css/'));
 });
+
+
+// Watch Task 
+// Watches for changes and triggers style task 
+gulp.task('watch', function() {
+    gulp.watch('scss/*.scss', ['styles']);
+});
+
+
+gulp.task('default', ['watch']);
