f033d64d8207a3faa1f45b892cc62f8a170c38f2
James Hanford <james@projkt.io>
2018-11-02 18:22:29 +1000

    Updated various out of date  content on the site.

--- js/content.js
+++ js/content.js
@@ -1,4 +1,4 @@
-var about_contents = "My name is James Hanford, I'm a 20 year old Computer Science undergraduate currently living in Brisbane. From an early age I have had a passion for everything technical, from computer programming to impress friends in school, to staying up late to watch livestreams of rocket launches. I find great pride within the code and tangible experiences I build for people, there is nothing more satisfying than watching an end user enjoy something that I have built. I aim to push boundaries in everyday life and enjoy life's little quirks inbetween.";
+var about_contents = "My name is James Hanford, I'm a 22 year old Computer Science graduate currently living in Brisbane. From an early age I have had a passion for everything technical, from computer programming to impress friends in school, to staying up late to watch livestreams of rocket launches. I find great pride within the code and tangible experiences I build for people, there is nothing more satisfying than watching an end user enjoy something that I have built. I aim to push boundaries in everyday life and enjoy life's little quirks inbetween.";
 
 $(document).ready(function () {
     
