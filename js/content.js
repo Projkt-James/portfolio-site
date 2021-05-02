19e3a861cdcb44b254c0bdcea0320080e3f59e9c
James Hanford <james@projkt.io>
2021-05-02 19:47:08 +1000

    Updated site content

--- js/content.js
+++ js/content.js
@@ -1,10 +1,9 @@
-var about_contents = "My name is James Hanford, I'm a 22 year old Computer Science graduate currently living in Brisbane. From an early age I have had a passion for everything technical, from computer programming to impress friends in school, to staying up late to watch livestreams of rocket launches. I find great pride within the code and tangible experiences I build for people, there is nothing more satisfying than watching an end user enjoy something that I have built. I aim to push boundaries in everyday life and enjoy life's little quirks inbetween.";
+var about_contents = `
+My name is James Hanford, I'm a 25 year old Fullstack Developer living in Brisbane, Australia. 
+I currently work as a member of a team bringing video content creation to the world through an in-browser editor called ClipChamp.
+From an early age I have had a passion for everything technical, since discovering computer programming towards the end of primary school, I've been hooked. I'm also an avid space enthusiast, mainly well read on the happenings of the current commerical resuable rockets space race.
+I find great pride within the code and tangible experiences I build for people, there is nothing more satisfying than watching an end user enjoy something that I have built.`;
 
 $(document).ready(function () {
-    
     $('.About-Centered p').append(about_contents);
-    
-    $('.About-Centered p').click(function () {
-        console.log("Clicked");
-    });
 });
