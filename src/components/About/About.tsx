34d2f0201dfc3f2337bb84d3e1ff5aabccec6bca
James Hanford <james@projkt.io>
2023-07-29 14:37:07 +1000

    chore: update content

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,9 +3,9 @@ import './About.scss';
 import profile from '../../assets/profile.jpg';
 
 const ABOUT_TEXT = `
-    My name is James Hanford, I'm a 25 year old Fullstack Developer living in Brisbane, Australia. 
+    My name is James Hanford, I'm a 27 year old Fullstack Developer living in Brisbane, Australia. 
     I currently work as a member of a team bringing video content creation to the world through an in-browser 
-    editor called ClipChamp. From an early age I have had a passion for everything technical, since 
+    editor called Clipchamp. From an early age I have had a passion for everything technical, since 
     discovering computer programming towards the end of primary school, I've been hooked. I'm also an 
     avid space enthusiast, mainly well read on the happenings of the current commerical reusable rockets 
     space race. I find great pride within the code and tangible experiences I build for people, there 
